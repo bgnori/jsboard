3685b3ac758d0fb563dd0163f75c96d296a4dc7f
Noryuki Hosaka <nori@shinano.tonic-water.com>
2009-11-26 22:02:04 +0900

    implelmented Area, DebugDump 	modified:   src/jsboard.js

--- src/jsboard.js
+++ src/jsboard.js
@@ -3,22 +3,51 @@
   Copyright 2009 Noriyuki Hosaka bgnori@gmail.com
 */
 
-/* 
 
-  $('#debug').val('debugger ready!\n');
-  $('#jsboard area').mousedown(function(){
-    var debug = $('#debug');
-    old = debug.val()
-    debug.val(old + 'mousedown at ' + $(this).attr("alt") + ' \n');
-    })
-  });
-*/
-debug_textarea = '<form><textarea cols="60" rows="5" id="debug"></textarea></form>';
 
-function Editor(n){
-  $(this).append(debug_textarea);
+function Image(r, gnubgid, height, width, css, usemap){
+  r.append('<img src="http://image.backgammonbase.com/image?gnubgid=4PPgAQPgc%2BQBIg%3AcAl7AAAAAAAA&height=262&width=341&css=nature&format=png" alt=gnubgid usemap="'+usemap+'" width="341" height="262" />');
 };
 
+function Area(map, id, shape, coords, alt){
+  map.append($('<a id="'+ id 
+             + '" shape="' + shape 
+             + '" coords="' + coords 
+             + '" alt="' + alt 
+             + '" nohref="nohref" />'));
+};
+
+function DebugDump(r, id){
+  debug_textarea = '<form><textarea cols="60" rows="5">debugger\n</textarea></form>';
+  r.append(debug_textarea);
+
+  r.find('#' +id +' a').mousedown(function(){
+    //alert($(this).attr("alt"));  //-> ok
+    debug = r.find("form textarea");
+    //alert(debug.attr('cols')); -> 60, ok
+    old = debug.val()
+    //debug.val(old + 'mousedown at hoge');
+    debug.val(old + 'mousedown at ' + $(this).attr("alt") + ' \n');
+    })
+};
+
+function Editor(n){
+  id_str =  'jsboard'+n;
+
+  root = $(this);
+
+  Image(root, 'gnubgid', 341, 252, 'css', '#'+id_str);
+
+  root.append($('<map name="' + id_str + '" id="'+ id_str + '" />'));
+  map = $('#' + id_str);
+  //alert(map.attr('id')); -> ok
+  Area(map, "13pt", "rect", "25,5,43,93", "13pt");
+  Area(map, "7pt", "rect", "115,139,133,227", "7pt");
+  Area(map, "yourbar", "rect", "133,116,158,228", "yourbar");
+
+  DebugDump(root, id_str);
+
+}
 
 $(document).ready(function(){
   $('.jsboard').each(Editor);
